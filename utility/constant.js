9eac986845d210dea49afd0e03d9b96963b2bb77
Ankush Sangwan <ankush2000sangwan@gmail.com>
2024-10-15 22:56:57 +0530

    multi page form

---
+++ utility/constant.js
@@ -0,0 +1,32 @@
+export const QUESTIONS = [
+    {
+      id: 1,
+      question_keyword: "name",
+      question: "What is your full name?",
+      type: "text"
+    },
+    {
+      id: 2,
+      question_keyword: "age",
+      question: "How old are you?",
+      type: "number"
+    },
+    {
+      id: 3,
+      question_keyword: "email",
+      question: "What's your email address?",
+      type: "email"
+    },
+    {
+      id: 4,
+      question_keyword: "birthdate",
+      question: "When is your birthday?",
+      type: "date"
+    },
+    {
+      id: 5,
+      question_keyword: "subscribe",
+      question: "Would you like to subscribe to our newsletter?",
+      type: "checkbox"
+    }
+  ]
